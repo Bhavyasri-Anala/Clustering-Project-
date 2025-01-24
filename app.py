a60f2dc988eeb676b6ed8545052c2284614caeb9
Bhavyasri-Anala <bhavyasri007@hotmail.com>
2025-01-24 11:04:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with open("pca.pkl","rb") as file:
     pca=pickle.load(file)
 
 def precition(input_data):
-    scaled_data=sc.transform(input_data)
+    scaled_data=scaler.transform(input_data)
     pca_data=pca.transform(scaled_data)
     pred=model.predict(pca_data)[0]
     if pred==0:
